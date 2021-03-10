15a0ac9e8d5361e529436b62ced51bc890254a19
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 18:05:49 -0800

    Created NavView and TabView structure in MovieSeriesView file

--- MovieSeries/MovieSeries/MovieSeriesView.swift
+++ MovieSeries/MovieSeries/MovieSeriesView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct MovieSeriesView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView {
+                
+            }
+        }
     }
 }
 
