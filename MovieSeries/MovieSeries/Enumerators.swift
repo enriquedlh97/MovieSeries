de1d45ed30bffb3e27218c120b92f80a684229ef
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 17:54:57 -0800

    Created rates enumerator

---
+++ MovieSeries/MovieSeries/Enumerators.swift
@@ -0,0 +1,19 @@
+//
+//  Enumerators.swift
+//  MovieSeries
+//
+//  Created by user189351 on 3/9/21.
+//
+
+import SwiftUI
+
+enum Rated: String {
+    case PG = "PG"
+    case PG13 = "PG13"
+    case B = "B"
+    case R = "R"
+    case M = "M"
+    case NR = "NR"
+    case TV14 = "TV14"
+    
+}
