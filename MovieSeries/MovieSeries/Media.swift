14877b14ba43e3a11a679b1553db8dd5ea423355
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 17:57:31 -0800

    Updated Media struct attributes

--- MovieSeries/MovieSeries/Media.swift
+++ MovieSeries/MovieSeries/Media.swift
@@ -12,5 +12,14 @@ struct Media: Identifiable {
     var id = UUID()
     var name: String
     var year: Int
+    var rated: Rated
+    var genre: String
+    var duration: String
+    var headline: String
+    var overview: String
+    var score: Double
+    var images: [String]
+    var logo: String
+    var trailer: String
     
 }
