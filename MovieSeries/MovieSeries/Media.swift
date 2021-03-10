a128d6fa54e8703dee91c558d139f73479bea2fb
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 17:51:57 -0800

    Created Media struct file

---
+++ MovieSeries/MovieSeries/Media.swift
@@ -0,0 +1,16 @@
+//
+//  Media.swift
+//  MovieSeries
+//
+//  Created by user189351 on 3/9/21.
+//
+
+import SwiftUI
+
+struct Media: Identifiable {
+    
+    var id = UUID()
+    var name: String
+    var year: Int
+    
+}
