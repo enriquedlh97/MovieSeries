7657d72c42d5ddebfcdc9f09a36d6daeba630956
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 18:08:22 -0800

    Created SeriesView file

---
+++ MovieSeries/MovieSeries/SeriesView.swift
@@ -0,0 +1,20 @@
+//
+//  SeriesView.swift
+//  MovieSeries
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/9/21.
+//
+
+import SwiftUI
+
+struct SeriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SeriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SeriesView()
+    }
+}
