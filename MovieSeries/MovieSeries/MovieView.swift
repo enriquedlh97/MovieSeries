1e258b5e02b52a53e3942314026e2c91deae2136
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-09 18:07:32 -0800

    Created MovieView file

---
+++ MovieSeries/MovieSeries/MovieView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieView.swift
+//  MovieSeries
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/9/21.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
