2f22018133e1fb99e638b9f21ea26c6d7186458b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-13 13:48:40 -0800

    Updated ScoreView to show circle with score

--- MovieSeries/MovieSeries/ScoreView.swift
+++ MovieSeries/MovieSeries/ScoreView.swift
@@ -5,16 +5,54 @@
 //  Created by Enrique Diaz de Leon Hicks on 3/13/21.
 //
 
+
 import SwiftUI
 
 struct ScoreView: View {
+    var score: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(Color.black)
+                .frame(width: 60, height: 60)
+            Circle()
+                .stroke(lineWidth: 10.0)
+                .opacity(0.55)
+                .foregroundColor(Color.black)
+                .frame(width: 70, height: 70)
+                
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(score / 100, 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                .foregroundColor(ScoreColor(score: score))
+                .rotationEffect(Angle(degrees: 270.0))
+                .frame(width: 70, height: 70)
+                .animation(.linear)
+            Text(String(format: "%.0f %%", min(score, 100.0)))
+                .font(.Roboto(size: 20))
+                .foregroundColor(.white)
+                .bold()
+        }
     }
+    
+    func ScoreColor(score: Double) -> Color {
+        if score < 60 {
+            return Color("ChiGong")
+        } else {
+            if score >= 60 && score < 85 {
+                return Color("MandarinSorbet")
+            } else {
+                return Color("Emerald")
+            }
+        }
+    }
+
+    
 }
 
 struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreView()
+        ScoreView(score: 70)
     }
 }
