6a9a1abd3aa2f9466f3a506b6bc696777e5f2ae7
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-13 14:51:19 -0800

    Created WebView file

---
+++ MovieSeries/MovieSeries/WebView.swift
@@ -0,0 +1,20 @@
+//
+//  WebView.swift
+//  MovieSeries
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/13/21.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView()
+    }
+}
