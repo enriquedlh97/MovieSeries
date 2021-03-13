9ef66b87b99b758c902363aa9d104ac4cc33a449
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-13 13:21:28 -0800

    Created extension Media for file preview

---
+++ MovieSeries/MovieSeries/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  MovieSeries
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/13/21.
+//
+
+import Foundation
+
+extension Media {
+    
+    public static var defaultMedia =  Media(name: "The Mandalorian", year: 2019, rated: .TV14, genre: "Sci-Fi & Fantasy, Action & Adventure, Western, Drama", duration: "35m", headline: "Bounty hunting is a complicated profession.", overview: "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.", score: 85, images: ["TheMandalorian1","TheMandalorian2","TheMandalorian3","TheMandalorian4","TheMandalorian5"], logo: "TheMandalorianLogo", trailer: "https://youtu.be/eW7Twd85m2g")
+    
+}
