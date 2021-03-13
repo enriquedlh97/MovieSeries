09354a9fd6b7fd26055976013f70d59dca745ffe
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-13 14:11:51 -0800

    Created mediaDetailView to show more details when a movie is clicked on

---
+++ MovieSeries/MovieSeries/MediaDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MediaDetailView.swift
+//  MovieSeries
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/13/21.
+//
+
+import SwiftUI
+
+struct MediaDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MediaDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaDetailView()
+    }
+}
