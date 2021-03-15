eb46f427127f02cd1e8bfb0d32b13db7c221161d
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-14 19:46:10 -0700

    Updated background colors in detail view

--- MovieSeries/MovieSeries/MediaDetailView.swift
+++ MovieSeries/MovieSeries/MediaDetailView.swift
@@ -14,69 +14,74 @@ struct MediaDetailView: View {
     var body: some View {
         GeometryReader { geo in
             
-            VStack {
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack {
-                        Text(media.overview)
-                            .font(.Roboto(size: 24))
-                            .foregroundColor(Color("Clouds"))
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 20)
-                            .padding(.top, 6)
-                        Text(media.genre)
-                            .font(.Roboto(size: 20))
-                            .foregroundColor(Color("Clouds"))
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 20)
-                            .padding(.top, 10)
-                        NavigationLink(
-                        
-                            destination: WebView(html: media.trailer),
-                            label: {
-                                Label("Trailer", systemImage: "play.tv.fill")
-                                    .font(.Roboto(size: 20))
-                                    .foregroundColor(Color("SwanWhite"))
-                                    .multilineTextAlignment(.center)
-                                    .background(Color("DraculaOrchid"))
-                                    .padding(.horizontal, 20)
-                                    .padding(.top, 10)
-                            })
-                        
-                    }
-                    VStack {
-                        TabView{
-                            ForEach(media.images, id: \.self) {
-                                image in
+            ZStack {
+                Color(.black)
+                VStack {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            Text(media.overview)
+                                .font(.Roboto(size: 24))
+                                .foregroundColor(Color("Clouds"))
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 20)
+                                .padding(.top, 6)
+                            Text(media.genre)
+                                .font(.Roboto(size: 20))
+                                .foregroundColor(Color("Clouds"))
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 20)
+                                .padding(.top, 10)
+                            NavigationLink(
+                            
+                                destination: WebView(html: media.trailer),
+                                label: {
+                                    Label("Trailer", systemImage: "play.tv.fill")
+                                        .font(.Roboto(size: 20))
+                                        .foregroundColor(Color("SwanWhite"))
+                                        .multilineTextAlignment(.center)
+                                        .background(Color("DraculaOrchid"))
+                                        .padding(.horizontal, 20)
+                                        .padding(.top, 10)
+                                        
+                                })
                                 
-                                Image(image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: geo.size.width-40)
-                                    .cornerRadius(40)
-                                    .overlay(
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 33)
-                                                .stroke(Color("SwanWhite"), style: StrokeStyle(lineWidth: 10, lineCap:  .round, lineJoin: .round))
-                                        }
-                                    )
-                                    .padding(.horizontal, 20)
-                                    .padding(.vertical,10)
-                                
-                                
-                            }
+                            
                         }
-                        .tabViewStyle(PageTabViewStyle())
+                        VStack {
+                            TabView{
+                                ForEach(media.images, id: \.self) {
+                                    image in
+                                    
+                                    Image(image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geo.size.width-40)
+                                        .cornerRadius(40)
+                                        .overlay(
+                                            ZStack {
+                                                RoundedRectangle(cornerRadius: 33)
+                                                    .stroke(Color(.black), style: StrokeStyle(lineWidth: 10, lineCap:  .round, lineJoin: .round))
+                                            }
+                                        )
+                                        .padding(.horizontal, 20)
+                                        .padding(.vertical,10)
+                                    
+                                    
+                                }
+                            }
+                            .tabViewStyle(PageTabViewStyle())
+                        }
                     }
                 }
+                .navigationBarTitle(media.name, displayMode: .inline)
+                .navigationBarColor(.black)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text(media.name)
+                            .font(.Roboto(size: 24))
+                            .foregroundColor(Color("DraculaOrchid"))
+                    }
             }
-            .navigationBarTitle(media.name, displayMode: .inline)
-            .navigationBarColor(UIColor(named: "DraculaOrchid"))
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text(media.name)
-                        .font(.Roboto(size: 24))
-                        .foregroundColor(Color("DraculaOrchid"))
-                }
             }
         }
     }
